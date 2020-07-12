5ecd4291779401cc2c91bb250a8a6ce75b39c152
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-07-11 21:25:42 -0500

    Add test_hash to use with my_each_with_index

--- main.rb
+++ main.rb
@@ -5,9 +5,16 @@ module Enumerable
     end
     self
   end
+  def my_each_with_index(item, index)
+    
+  end
 end
 
 test = %w[Marcos Samuel Ruby Rails Code Terminal]
+test_hash = { name: 'Ruby', age: '25', height: '10' }
 
 test.my_each { |itr| puts itr }
 test.each { |itr| puts itr }
+
+test_hash.my_each_with_index {}
+test.each_with_index
