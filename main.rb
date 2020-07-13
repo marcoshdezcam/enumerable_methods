0e9b8b8b33a1ec5027a165a9c27ee353da26232b
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-07-13 12:39:07 -0500

    Fix Rubocop issues && Add ternary operator to my_select

--- main.rb
+++ main.rb
@@ -6,31 +6,21 @@ module Enumerable
     self
   end
 
-
   def my_each_with_index
-    item = Hash.new(self)
     for i in self do
       yield(i)
     end
-    self    
+    self
   end
 
   def my_select
     item = []
     for i in self do
-      if yield(i) == true
-        item <<  i
-      end         
-    end  
-    item  
+      yield(i) ? item << i : nil
+    end
+    item
   end
-
-
-
-
-
 end
-
 
 test = %w[Marcos Samuel Ruby Rails Code Terminal]
 test_hash = { name: 'Ruby', age: '25', height: '10' }
@@ -38,7 +28,8 @@ test_hash = { name: 'Ruby', age: '25', height: '10' }
 # p test.my_each { |itr| puts itr }
 # p test.each { |itr| puts itr }
 
-# p test_hash.my_each_with_index {|itr| puts itr}
-# p test_hash.each_with_index {|itr| puts itr}
+# p test_hash.my_each_with_index { |itr| puts itr}
+# p test_hash.each_with_index { |itr| puts itr}
 
-# p test_hash.my_select {|itr| itr == 'Ruby'}
+p test.my_select { |itr| itr == 'Ruby' }
+p test.select { |itr| itr == 'Ruby' }
