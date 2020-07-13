cc3db3838c7561d772ee18bd7164a389a1163e6a
Samuel <samuelalmaoherrera@gmail.com>
2020-07-13 11:19:56 -0300

    added select and each_with_index.

--- main.rb
+++ main.rb
@@ -5,16 +5,40 @@ module Enumerable
     end
     self
   end
-  def my_each_with_index(item, index)
-    
+
+
+  def my_each_with_index
+    item = Hash.new(self)
+    for i in self do
+      yield(i)
+    end
+    self    
   end
+
+  def my_select
+    item = []
+    for i in self do
+      if yield(i) == true
+        item <<  i
+      end         
+    end  
+    item  
+  end
+
+
+
+
+
 end
+
 
 test = %w[Marcos Samuel Ruby Rails Code Terminal]
 test_hash = { name: 'Ruby', age: '25', height: '10' }
 
-test.my_each { |itr| puts itr }
-test.each { |itr| puts itr }
+# p test.my_each { |itr| puts itr }
+# p test.each { |itr| puts itr }
 
-test_hash.my_each_with_index {}
-test.each_with_index
+# p test_hash.my_each_with_index {|itr| puts itr}
+# p test_hash.each_with_index {|itr| puts itr}
+
+# p test_hash.my_select {|itr| itr == 'Ruby'}
