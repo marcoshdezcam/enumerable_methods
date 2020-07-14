0c9c8382f74918c88c89d4459aa49ca97884b086
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-07-14 12:34:31 -0500

    Add my_map. Fix counters on methods. Change testcases for my_count

--- main.rb
+++ main.rb
@@ -22,12 +22,11 @@ module Enumerable
   end
 
   def my_all?
-    len = self.length
     counter = 0
     for i in self do
       yield(i) ? counter += 1 : nil
     end
-    counter == len ? true : false
+    counter == self.length
   end
 
   def my_any?
@@ -35,7 +34,7 @@ module Enumerable
     for i in self do
       yield(i) ? counter += 1 : nil
     end
-    counter > 0 ? true : false
+    counter.positive?
   end
 
   def my_none?
@@ -43,23 +42,31 @@ module Enumerable
     for i in self do
       yield(i) ? counter += 1 : nil
     end
-    counter == 0 ? true : false
+    counter.zero?
   end
 
-  def my_count?
-    item = []
+  def my_count
     counter = 0
-    for i in self do
-      yield(i) ?  item << i counter += 1 : nil
+    if block_given?
+      for i in self do
+        yield(i) ? counter += 1 : nil
+      end
+      counter
+    else
+      self.size
     end
-    item
   end
 
-  
-
+  def my_map
+    new_array = []
+    for i in self do
+      yield(i) ? new_array << i : new_array << false
+    end
+    new_array
+  end
 end
 
-test = %w[ Ruby Marcos Patata Hipotenusa Sam ]
+test = %w[Ruby Marcos Patata Hipotenusa Sam]
 test_hash = { name: 'Ruby', age: '25', height: '10' }
 
 # p test.my_each { |itr| puts itr }
@@ -77,5 +84,13 @@ test_hash = { name: 'Ruby', age: '25', height: '10' }
 # p test.my_any? { |itr| itr == 'papa' }
 # p test.my_any? { |itr| itr == 'Ruby' }
 
-p test.my_none? { |itr| itr == 'papa' }
-p test.my_none? { |itr| itr == 'Ruby' }
+# p test.my_none? { |itr| itr == 'papa' }
+# p test.my_none? { |itr| itr == 'Ruby' }
+
+# p test.my_count
+# p test.my_count { |itr| itr == "Marcos" }
+# p test.count
+# p test.count { |itr| itr == "Marcos" }
+
+p test.my_map { |itr| itr == "Hola"}
+p test.map { |itr| itr == "Hola"}
