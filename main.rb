cc44bb9a7c14686e218dc2168183b3ba9e74eedb
Samuel <samuelalmaoherrera@gmail.com>
2020-07-14 10:47:04 -0300

    Finished any and none methods

--- main.rb
+++ main.rb
@@ -20,9 +20,46 @@ module Enumerable
     end
     item
   end
+
+  def my_all?
+    len = self.length
+    counter = 0
+    for i in self do
+      yield(i) ? counter += 1 : nil
+    end
+    counter == len ? true : false
+  end
+
+  def my_any?
+    counter = 0
+    for i in self do
+      yield(i) ? counter += 1 : nil
+    end
+    counter > 0 ? true : false
+  end
+
+  def my_none?
+    counter = 0
+    for i in self do
+      yield(i) ? counter += 1 : nil
+    end
+    counter == 0 ? true : false
+  end
+
+  def my_count?
+    item = []
+    counter = 0
+    for i in self do
+      yield(i) ?  item << i counter += 1 : nil
+    end
+    item
+  end
+
+  
+
 end
 
-test = %w[Marcos Samuel Ruby Rails Code Terminal]
+test = %w[ Ruby Marcos Patata Hipotenusa Sam ]
 test_hash = { name: 'Ruby', age: '25', height: '10' }
 
 # p test.my_each { |itr| puts itr }
@@ -31,5 +68,14 @@ test_hash = { name: 'Ruby', age: '25', height: '10' }
 # p test_hash.my_each_with_index { |itr| puts itr}
 # p test_hash.each_with_index { |itr| puts itr}
 
-p test.my_select { |itr| itr == 'Ruby' }
-p test.select { |itr| itr == 'Ruby' }
+# p test.my_select { |itr| itr == 'Ruby' }
+# p test.select { |itr| itr == 'Ruby' }
+
+# p test.my_all? { |itr| itr == 'papa' }
+# p test.my_all? { |itr| itr == 'Ruby' }
+
+# p test.my_any? { |itr| itr == 'papa' }
+# p test.my_any? { |itr| itr == 'Ruby' }
+
+p test.my_none? { |itr| itr == 'papa' }
+p test.my_none? { |itr| itr == 'Ruby' }
