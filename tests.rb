c8b270dd6c0eb59463a5138552b9dd2978bb9106
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-07-15 15:20:56 -0500

    Add variables to tests.rb

--- tests.rb
+++ tests.rb
@@ -1,5 +1,7 @@
 load 'main.rb'
 # Test variables
+t_name = 'my_each'
+t_comp = 'each'
 
 test_string = %w[Ruby Marcos Patata Hipotenusa Sam]
 test_numbers = [1, 2, 3, 4, 5]
@@ -7,53 +9,53 @@ test_range = 5...10
 test_hash = { name: 'Ruby', age: '25', height: '10' }
 
 # Testing Each/My_Each
-puts %(my_each)
-p test_string.my_each
+puts t_name
+p test_string.meth.call
 puts %()
-puts %(each)
+puts t_comp
 p test_string.each
 puts %()
-puts %(my_each)
+puts t_name
 p test_string.my_each { |itr| puts itr }
 puts %()
-puts %(each)
+puts t_comp
 p test_string.each { |itr| puts itr }
 puts %()
 
-puts %(my_each)
+puts t_name
 p test_numbers.my_each
 puts %()
-puts %(each)
+puts t_comp
 p test_numbers.each
 puts %()
-puts %(my_each)
+puts t_name
 p test_numbers.my_each { |itr| puts itr }
 puts %()
-puts %(each)
+puts t_comp
 p test_numbers.each { |itr| puts itr }
 puts %()
 
-puts %(my_each)
+puts t_name
 p test_range.my_each
 puts %()
-puts %(each)
+puts t_comp
 p test_range.each
 puts %()
-puts %(my_each)
+puts t_name
 p test_range.my_each { |itr| puts itr }
 puts %()
-puts %(each)
+puts t_comp
 p test_range.each { |itr| puts itr }
 puts %()
 
-puts %(my_each)
+puts t_name
 p %(my_each), test_hash.my_each
 puts %()
-puts %(each)
+puts t_comp
 p %(each), test_hash.each
 puts %()
-puts %(my_each)
+puts t_name
 p %(my_each), test_hash.my_each { |itr| puts itr }
 puts %()
-puts %(each)
+puts t_comp
 p %(each), test_hash.each { |itr| puts itr }
