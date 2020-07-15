512317d34fc6d5a980cb211e264542b201588eed
Samuel <samuelalmaoherrera@gmail.com>
2020-07-15 13:14:00 -0300

    Set work evioroment for testing methods.

--- main.rb
+++ main.rb
@@ -1,127 +1,44 @@
 module Enumerable
-  def my_each()
+  def my_each
     return to_enum(:my_each) unless block_given?
 
     arr = self
+    tarr = arr.to_a
     i = 0
-    while i < arr.size
-      yield arr[i]
+    while i < tarr.size
+      yield tarr[i]
       i += 1
     end
     self
   end
-
-  def my_each_with_index
-    return to_enum(:my_each_with_index) unless block_given?
-
-    index = 0
-    for i in self do
-      yield(i, index)
-      index += 1
-    end
-    self
-  end
-
-  def my_select
-    item = []
-    for i in self do
-      yield(i) ? item << i : nil
-    end
-    item
-  end
-
-  def my_all?(*args)
-    counter = 0
-    size = self.size
-    for i in self do
-      yield(i) ? counter += 1 : nil
-    end
-    counter == size
-  end
-
-  def my_any?
-    counter = 0
-    for i in self do
-      yield(i) ? counter += 1 : nil
-    end
-    counter.positive?
-  end
-
-  def my_none?
-    counter = 0
-    for i in self do
-      yield(i) ? counter += 1 : nil
-    end
-    counter.zero?
-  end
-
-  def my_count
-    counter = 0
-    size = self.size
-    if block_given?
-      for i in self do
-        yield(i) ? counter += 1 : nil
-      end
-      counter
-    else
-      size
-    end
-  end
-
-  def my_map
-    new_array = []
-    for i in self do
-      new_array << yield(i)
-    end
-    new_array
-  end
-
-  def my_inject
-    for i in self do
-      yield(sum, i[i - 1])
-    end
-    sum
-  end
 end
+
+# Test variables
 
 test_string = %w[Ruby Marcos Patata Hipotenusa Sam]
 test_numbers = [1, 2, 3, 4, 5]
 test_range = 5...10
 test_hash = { name: 'Ruby', age: '25', height: '10' }
 
-p test_string.my_each { |itr| puts itr }
-p test_string.each { |itr| puts itr }
+# Testing Each/My_Each
+puts %(Testing my_each)
+
 p test_string.my_each
+p test_string.my_each { |itr| puts itr }
+p test_numbers.my_each
+p test_numbers.my_each { |itr| puts itr }
+p test_range.my_each
+p test_range.my_each { |itr| puts itr }
+p test_hash.my_each
+p test_hash.my_each { |itr| puts itr }
+
+puts %(Testing each)
+
 p test_string.each
-# p test_numbers.my_each { |itr| puts itr }
-# p test_numbers.each { |itr| puts itr }
-# p test_range.my_each { |itr| puts itr }
-# p test_range.each { |itr| puts itr }
-# p test_hash.my_each { |itr| puts itr }
-# p test_hash.each { |itr| puts itr }
-
-
-# p test_hash.my_each_with_index { |item, index| puts %(#{item} on #{index} position) }
-# p test_hash.each_with_index { |item, index| puts %(#{item} on #{index} position) }
-
-# p test_hash.my_select { |itr| itr == 'Ruby' }
-# p test_hash.select { |itr| itr == 'Ruby' }
-
-# p test_string.my_all? { |itr| itr == 'papa' }
-# p test_string.my_all? { |itr| itr == 'Ruby' }
-# p test_string.all? {}
-
-# p test_string.my_any? { |itr| itr == 'papa' }
-# p test_string.my_any? { |itr| itr == 'Ruby' }
-
-# p test_string.my_none? { |itr| itr == 'papa' }
-# p test_string.my_none? { |itr| itr == 'Ruby' }
-
-# p test_string.my_count { |itr| itr == "Marcos" }
-# p test_string.count { |itr| itr == "Marcos" }
-
-# p test_numbers.my_map { |itr| itr * 2}
-# p test_numbers.map { |itr| itr * 2}
-
-# p test_numbers.my_inject { |sum, number| sum + number }
-# p test_numbers.inject { |sum, number| sum + number }
+p test_string.each { |itr| puts itr }
+p test_numbers.each
+p test_numbers.each { |itr| puts itr }
+p test_range.each
+p test_range.each { |itr| puts itr }
+p test_hash.each
+p test_hash.each { |itr| puts itr }
---
+++ seudo.rb
@@ -0,0 +1,122 @@
+def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    index = 0
+    arr = self
+    i = 0
+    while i < arr.size
+      yield(i, index)
+      index += 1
+      i += 1
+    end
+    self
+  end
+
+  def my_select
+    item = []
+    arr = self
+    i = 0
+    while i < arr.size
+      yield(i) ? item << i : nil
+      i += i
+    end
+    item
+  end
+
+  def my_all?(*args)
+    counter = 0
+    size = self.size
+    i = 0
+    while i < size
+      yield(i) ? counter += 1 : nil
+      i += 1
+    end
+    counter == size
+  end
+
+  def my_any?
+    counter = 0
+    size = self.size
+    i = 0
+    while i < size
+      yield(i) ? counter += 1 : nil
+      i += 1
+    end
+    counter.positive?
+  end
+
+  def my_none?
+    counter = 0
+    size = self.length
+    i = 0
+    while i < size
+      yield(i) ? counter += 1 : nil
+      i += 1
+    end
+    counter.zero?
+  end
+
+  def my_count
+    counter = 0
+    size = self.size
+    i += 0
+      while i < size 
+        yield(i) ? counter += 1 : nil
+        i += 1
+      end
+      counter
+    else
+      size
+    end
+  end
+
+  def my_map
+    new_array = []
+    size = self.size
+    i = 0
+    while i < size 
+      new_array << yield(i)
+      i += 1
+    end
+    new_array
+  end
+
+  def my_inject
+    size = self.size
+    i = 0
+    while i < size 
+      yield(sum, i[i - 1])
+      i += 1
+    end
+    sum
+  end
+end
+
+
+# p test_hash.my_each_with_index { |item, index| puts %(#{item} on #{index} position) }
+# p test_hash.each_with_index { |item, index| puts %(#{item} on #{index} position) }
+
+# p test_hash.my_select { |itr| itr == 'Ruby' }
+# p test_hash.select { |itr| itr == 'Ruby' }
+
+# p test_string.my_all? { |itr| itr == 'papa' }
+# p test_string.my_all? { |itr| itr == 'Ruby' }
+# p test_string.all? {}
+
+# p test_string.my_any? { |itr| itr == 'papa' }
+# p test_string.my_any? { |itr| itr == 'Ruby' }
+#   test_string.my_any? { |itr| itr == 'papa' }
+# p test_string.my_any? { |itr| itr == 'Ruby' }
+# p 
+
+# p test_string.my_none? { |itr| itr == 'papa' }
+# p test_string.my_none? { |itr| itr == 'Ruby' }
+
+# p test_string.my_count { |itr| itr == "Marcos" }
+# p test_string.count { |itr| itr == "Marcos" }
+
+# p test_numbers.my_map { |itr| itr * 2}
+# p test_numbers.map { |itr| itr * 2}
+
+# p test_numbers.my_inject { |sum, number| sum + number }
+# p test_numbers.inject { |sum, number| sum + number }
