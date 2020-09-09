7b9877da2d462a8170b6b34a115348fd1857f695
solbencas <cecibenitezca@gmail.com>
2020-09-09 13:47:59 -0400

    Add hash test to #my_each:
    Add the test to check if the my_each method returns the values of a hash.

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,6 +3,7 @@ require_relative '../main'
 describe Enumerable do 
   let(:array) { [1, 2, 3, 4, 5, 6, 7] }
   let(:range) { (1..7) } 
+  let(:hash) { { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7} }
 
   describe "#my_each" do 
     it "Printing array elements" do 
@@ -11,6 +12,10 @@ describe Enumerable do
 
     it 'Printing the elements in the range' do
       expect { range.my_each { |x| puts x } }.to output("1\n2\n3\n4\n5\n6\n7\n").to_stdout
+    end 
+
+    it 'Printing the elements in a hash' do
+      expect { hash.my_each { |k, v| puts v } }.to output("1\n2\n3\n4\n5\n6\n7\n").to_stdout
     end
   end
 end
