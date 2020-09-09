f409dff71fb7162961e397bfa6ecdc0d445a2716
solbencas <cecibenitezca@gmail.com>
2020-09-09 13:56:49 -0400

    Add array of strings to #my_each:
    Add the array_of_strings variable and test the output with the my_each method.

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,6 +4,7 @@ describe Enumerable do
   let(:array) { [1, 2, 3, 4, 5, 6, 7] }
   let(:range) { (1..7) } 
   let(:hash) { { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7} }
+  let(:array_of_strings) { %w[Hello World Ruby Ceci Marcos]}
 
   describe "#my_each" do 
     it "Printing array elements" do 
@@ -24,6 +25,10 @@ describe Enumerable do
 
     it 'Check if returns the enumerator' do
       expect(array.my_each).to be_an_instance_of Enumerator
+    end
+
+    it 'should print the elements in the array of strings' do
+      expect { array_of_strings.my_each { |x| puts x } }.to output("Hello\nWorld\nRuby\nCeci\nMarcos\n").to_stdout
     end
   end
 end
